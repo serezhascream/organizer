581e2b1960f61d6956b0a7d73680a14aa6414489
Sergei Matiushenko <csserezha@gmail.com>
2021-08-08 21:12:36 +0300

    test: add test for getDayClass method in common utils

---
+++ src/data/fixture.ts
@@ -0,0 +1,11 @@
+import { TDayObject } from './types';
+
+export const day:TDayObject = {
+	day: 1,
+	month: 'current',
+	isToday: false,
+	timestamp: 1000000000,
+	weekday: 1,
+	isWeekend: false,
+	isSelected: false,
+};
