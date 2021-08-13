746c240f143cc092fc49a4252189438b7b0e1aa6
Sergei Matiushenko <csserezha@gmail.com>
2021-08-13 13:43:13 +0300

    test: add check for switching to prev month
    faced a problem when act doesn't work correctly.

--- src/hooks/__tests__/useDecade.test.ts
+++ src/hooks/__tests__/useDecade.test.ts
@@ -1,29 +1,31 @@
-import { describe, expect, it } from '@jest/globals';
-import { renderHook, act } from '@testing-library/react-hooks';
+import { describe, expect, it, afterEach } from '@jest/globals';
+import { renderHook, act, cleanup } from '@testing-library/react-hooks';
 
 import { getDecade, useDecade } from '../useDecade';
-import { eighties_decade, nineties_decade } from '../../data/fixture';
+import { eighties_decade, seventies_decade } from '../../data/fixture';
 
 describe('hooks > useDecade', () => {
+	afterEach(cleanup);
+	
 	it('getDecade function returns correct decade for 1984', () => {
 		const decade = getDecade(1984);
 		
 		expect(decade).toEqual(eighties_decade);
 	});
+	
 	it('hook returns correct decade for 1984', () => {
 		const { result } = renderHook(() => useDecade(1984));
-		const [decade] = result.current;
 		
-		expect(decade).toEqual(eighties_decade);
+		expect(result.current.decade).toEqual(eighties_decade);
 	});
-	it('hook switches decade to 90s', () => {
+	
+	it.todo('hook switches decade to 90s');
+	
+	it('hook switches decade to 70s', () => {
 		const { result } = renderHook(() => useDecade(1984));
-		const [decade, switchDecade] = result.current;
 		
-		act(() => {
-			switchDecade('next');
-		})
+		act(() => result.current.switchDecade('prev'));
 		
-		expect(decade).toEqual(nineties_decade);
+		expect(result.current.decade).toEqual(seventies_decade);
 	});
 });
