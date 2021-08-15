d560fe2cda7f2921b566fb6edf828d495b99d549
Sergei Matiushenko <csserezha@gmail.com>
2021-08-15 14:51:19 +0300

    refactor: refactor styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,19 +18,26 @@ const Organizer = ({ firstDayIsMonday = true }: TAppProps) => {
 	
 	return (
 		<div className="org-wrapper">
-			<div className="org-header">
+			<section className="org-header">
 				<h1 className="org-header__title">Organizer</h1>
-			</div>
-			<div className="org-content">
-				<div className="org-content__calendar">
+			</section>
+			<div className="org-divider" />
+			<section className="org-container">
+				<div className="org-container__calendar">
 					<Calendar
 						firstDayIsMonday={firstDayIsMonday}
 						selected={selected}
 						onSelectDay={handlerSelectDay}
 					/>
 				</div>
-				<div className="org-content__event-list" />
-			</div>
+				<div className="org-container__divider" />
+				<div className="org-container__content">
+					<div className="org-container__content-title">
+						Events
+					</div>
+					<div className="org-container__content-list"></div>
+				</div>
+			</section>
 		</div>
 	);
 };
