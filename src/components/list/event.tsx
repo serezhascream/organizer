a5796f0dae6530743bdfa4c887fe57f4f540a49e
Sergei Matiushenko <csserezha@gmail.com>
2021-10-04 14:34:05 +0300

    feat: add a delete button and move event styles to a separate file

--- src/components/list/event.tsx
+++ src/components/list/event.tsx
@@ -5,28 +5,43 @@ import Icon from '../ui-kit/icon';
 import { TEventObj } from '../../data/types';
 
 interface Props extends TEventObj {
-	onEditEvent(eventId: number): void,
+	onEditEvent(eventId: number): void;
+	onDeleteEvent(eventId: number): void
 }
 
 const Event: React.VFC<Props> = (props: Props) => {
-	const { id, title, description, onEditEvent } = props;
+	const {
+		id,
+		title,
+		description,
+		onEditEvent = () => {},
+		onDeleteEvent = () => {},
+	} = props;
 	const descriptionText = React.useMemo(
 		(): string => (description || 'Empty description'),
 		[event]
 	);
 
 	const handlerEditEvent = React.useCallback(() => onEditEvent(id), [id, onEditEvent]);
+	const handlerDeleteEvent = React.useCallback(() => onDeleteEvent(id), [id, onDeleteEvent]);
 	
 	return (
 		<div className="org-event">
 			<div className="org-event__title">{ title }</div>
 			<div className="org-event__description">{ descriptionText }</div>
 
-			<Icon
-				name="edit"
-				className="org-event__edit-button"
-				onClick={handlerEditEvent}
-			/>
+			<div className="org-event__buttons">
+				<Icon
+					name="edit"
+					className="org-event__button"
+					onClick={handlerEditEvent}
+				/>
+				<Icon
+					name="delete"
+					className="org-event__button"
+					onClick={handlerDeleteEvent}
+				/>
+			</div>
 		</div>
 	);
 };
