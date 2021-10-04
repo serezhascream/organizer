621043e49c98559b1ebf45dc83f87380286a090a
Sergei Matiushenko <csserezha@gmail.com>
2021-10-04 15:44:21 +0300

    refactor: rename App to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import ReactFlexCalendar from './components/App';
+import ReactFlexCalendar from './components/';
 import { Provider } from 'react-redux';
 
 import store from './store';
