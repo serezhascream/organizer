0e39d71b7fd6cf67e25636d29f9b7efd0289835a
Sergei Matiushenko <csserezha@gmail.com>
2021-10-17 20:34:58 +0300

    tests: add fixture for localStorage

---
+++ src/tests/fixture/localStorage.ts
@@ -0,0 +1,25 @@
+export default {
+	orgCalendarEvents: [
+		{
+			id: "UPZTSqx_LhyTkmaqfOq5a",
+			timestamp: 1634541120000,
+			title: "Meetup @ Expo",
+			description: "Free food. I should be there!",
+			hasTime: false
+		},
+		{
+			id: "tL4E3ytGaBYjgPFH-c-QB",
+			timestamp: 1634112000000,
+			title: "A very long title for this amazing event just to check how does it fit in list view and nothing more...",
+			description: "And some short description",
+			hasTime: true
+		},
+		{
+			id: "nnYNmvGBJkrCl2KzV9PHc",
+			timestamp: 1634282484406,
+			hasTime: false,
+			title: "Alex's birthday",
+			description: "asdfkasdflj lasdfklkj"
+		}
+	],
+};
